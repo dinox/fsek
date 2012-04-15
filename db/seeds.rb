98bb96a039002017d68472dee98a4134803b4238
Linus Hammarlund <linus@mint>
2012-04-15 21:47:47 +0200

    Seeds för news.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,35 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+
+News.delete_all
+News.create(
+	:title => 'Valborg i Uppsala',
+	:text =>
+		"Även fast det senast påskafton snöade här i vårt kära Lund
+så är, tro det eller ej, våren på väg. Förutom solsken, blommor och bin kommer
+även Valborg med våren. Denna högtid firas traditionsenligt den 30 april för
+att hälsa våren välkommen genom att festa som as! Att festa som as görs bäst på
+bortaplan därför anordnas det liksom i fjol en resa till Uppsala över Valborg.
+Det studentikosa Uppsala erbjuder allt från fulsittning till forsränning under
+Valborg! Vi lämnar Lund den 29e april och reser hem igen den 1a maj. För
+anmälan och mer information kontakta:"
+	)
+
+News.create(
+	:title => 'Hälsovecka på F',
+	:text => %{Nu är det äntligen dags för årets hälsovecka på F-sektionen!
+En chans för just dig att lära dig mer om hälsa och testa på nya roliga
+aktiviteter. Mellan den 17:e och den 23:e april erbjuder F-sektionens
+idrottsutskott sektionens medlemmar att prova på och utveckla sin hälsa genom
+ett flertal olika aktiviteter.}
+	)
+News.create(
+	:title => 'Störningar på hemsidan',
+	:text => %{Den kommande veckan så kan det förekomma störningar på
+hemsidan såsom att den ligger nere helt. Detta beror på renoveringar av
+sektionens lokaler men förhoppnigsvis kommer detta endast att gälla begränsade
+tider.  Vid uthämtning av nycklar till F-bilen kontakta Björn Hansson,
+telefonnummer går att hitta under "Styrelsen" till vänster på hemsidan eller
+anslaget på SK's dörr.}
+	)
