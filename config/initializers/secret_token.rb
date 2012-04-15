33838b6a919b769b0203a753ece0ea65eb071a11
Johan Förberg <johan@forberg.se>
2012-04-15 16:55:25 +0200

    Init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fsek::Application.config.secret_token = 'f7b049aa18c835dafa315336f7b3005b70a7e816cde6d4d49ac670d477b2617fd55243a56af94f2c676e1b4ad5b791a5a67a50ee705d5298217ce6d2cc683674'
