d877b6fd1efe5de1e1f7abdf206b6d2115694fb3
Linus Hammarlund <linus.snail@gmail.com>
2012-04-16 03:28:32 +0200

    Merge.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Fsek::Application.routes.draw do
+	controller :sessions do 
+		get 'login' => :new
+		post 'login' => :create
+		delete 'logout' => :destroy
+	end
+
   resources :users
 
   resources :news
