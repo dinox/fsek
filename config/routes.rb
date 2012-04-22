c1c8afd06a057fd3230db00a4d6b0a7a8439f2f4
Linus Hammarlund <linus.snail@gmail.com>
2012-04-22 22:44:01 +0200

    Ny routing för login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Fsek::Application.routes.draw do
-  devise_for :users
+  resources :roles
 
-  resources :accesses
 
-  resources :posts
 
   resources :users
 
   resources :news
+
+  devise_for :users, :skip => [:sessions]
+  as :user do
+    get 'login' => 'devise/sessions#new', :as => :new_user_session
+    post 'login' => 'devise/sessions#create', :as => :user_session
+    delete 'logout' => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
