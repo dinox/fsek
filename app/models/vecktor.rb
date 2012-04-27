585b26440bc59fb46e3b4286bca9348befa22cc5
Johan Forberg <johan@forberg.se>
2012-04-27 20:31:56 +0200

    Revert "Byter namn på kolumnen year => date, typ date"
    This reverts commit c1072708dfbf2a149a44ce766468d33c96163145.

--- app/models/vecktor.rb
+++ app/models/vecktor.rb
@@ -3,7 +3,7 @@ require 'fsek_settings'
 class Vecktor < ActiveRecord::Base
   include FsekSettings
 
-  attr_accessible :editor, :issue, :responsible_publisher, :date
+  attr_accessible :editor, :issue, :responsible_publisher, :year
   
   has_many :vecktor_notices
 
