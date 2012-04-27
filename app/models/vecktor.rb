5eee0e41072bca22c2af7d14bdb233f0a5186d96
Johan Forberg <johan@forberg.se>
2012-04-27 20:45:10 +0200

    Default-värden för vecktorns fält.

--- app/models/vecktor.rb
+++ app/models/vecktor.rb
@@ -3,10 +3,17 @@ require 'fsek_settings'
 class Vecktor < ActiveRecord::Base
   include FsekSettings
 
-  attr_accessible :editor, :issue, :responsible_publisher, :year
+  attr_accessible :editor, :issue, :responsible_publisher, :date
   
   has_many :vecktor_notices
 
+  before_save :default_values # Kallback innan saker kommittas till databasen
+
+  def default_values
+    self.date      ||= Time.now.to_date # Vi bryr oss inte om tiden.
+    self.editor    ||= Vecktor.setting :editor
+    self.publisher ||= Vecktor.setting :publisher
+  end
 end
 
 class VecktorNotice < ActiveRecord::Base
