c1072708dfbf2a149a44ce766468d33c96163145
Johan Forberg <johan@forberg.se>
2012-04-27 20:30:25 +0200

    Byter namn på kolumnen year => date, typ date

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120424115148) do
+ActiveRecord::Schema.define(:version => 20120427181848) do
 
   create_table "news", :force => true do |t|
     t.string   "title"
@@ -84,12 +84,12 @@ ActiveRecord::Schema.define(:version => 20120424115148) do
   end
 
   create_table "vecktors", :force => true do |t|
-    t.integer  "year"
     t.integer  "issue"
     t.string   "editor"
     t.string   "publisher"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.date     "date"
   end
 
 end
