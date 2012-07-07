a123023cc68293199f1fc7ca80df3cda2e6261fe
Johan Förberg <johan@forberg.se>
2012-07-06 21:31:21 -0400

    Kontroll och actionnamnet tillgängligt i viewar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 # encoding=utf-8
 class ApplicationController < ActionController::Base
-	before_filter :authenticate_user!
+	before_filter :do_before_filter
 	protect_from_forgery
 	check_authorization :unless => :devise_controller?
+
+    def do_before_filter
+      authenticate_user!
+      @current_action = action_name
+      @current_controller = controller_name
+    end
+
 =begin
 	rescue_from CanCan::AccessDenied do |exception|
 		redirect_to root_url, :alert => exception.message
