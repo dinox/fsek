372e3cfb26a8e856f9fb77cad8d0508795c5545f
Linus Hammarlund <linus.snail@gmail.com>
2012-08-17 02:38:51 +0200

    Routade menyn

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
-  skip_before_filter:authenticate_user!, :only => [:show]
+  skip_before_filter:authenticate_user!, :only => [:show, :index]
   load_and_authorize_resource
+
   # GET /pages
   # GET /pages.json
   def index
